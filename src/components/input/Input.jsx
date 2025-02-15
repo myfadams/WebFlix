4653992268848a578d2e136b2d1741732da48404
fadl <faadamms@gmail.com>
2025-02-15 14:22:30 +0000

    made the sign up page and made it responsive too

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./input.module.css"
-function Input() {
+function Input({value,setValue,placeHolder, type,name}) {
+    const [shouldHide,setShouldHide]=useState(true)
   return (
-    <div>
-      <input type="text" className={styles.mainIn}/>
-    </div>
-  )
+		<div className={styles.mainIn}>
+			<input
+				type={type=="password"?(!shouldHide?"text":"password"):type}
+				placeholder={placeHolder}
+				onChange={(e) => {
+					const val = e.target.value;
+					setValue({ ...value, [name]: val });
+				}}
+				value={value[name]}
+                // hidden=
+			/>
+			{(name === "password" || name === "confirmPassword") && (
+				<div className={styles.touchableOpacity}
+					// style={}
+                    onClick={()=>{
+                        setShouldHide(!shouldHide)
+                    }}
+				>
+					{!shouldHide ? (
+						<img src="./hide.png" alt="" />
+					) : (
+						<img src="./show.png" alt="" />
+					)}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default Input
--- src/screens/SignUp.jsx
+++ src/screens/SignUp.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import styles from "./sign-up.module.css"
-import Input from '../components/input/Input';
+import React, { useState } from "react";
+import styles from "./sign-up.module.css";
+import Input from "../components/input/Input";
+import { NavLink } from "react-router";
 
 function SignUp() {
-  return (
+	const [userDetails, setUserDetails] = useState({
+		username: "",
+		email: "",
+		password: "",
+		confirmPassword: "",
+	});
+	const [remember, setRemember] = useState(false);
+	return (
 		<section className={styles.signMain}>
 			<div className={styles.mainOverlay}></div>
 			<div>
@@ -12,15 +20,84 @@ function SignUp() {
 				</div>
 				<div className={styles.signBody}>
 					{/* <div className=''> */}
-						<form action="">
-							<h2>Sign in</h2>
-							<Input />
-						</form>
+					<form action="">
+						<h2>Sign up</h2>
+						<Input
+							placeHolder={"Username"}
+							setValue={setUserDetails}
+							type={"text"}
+							value={userDetails}
+							name={"username"}
+						/>
+						<Input
+							placeHolder={"Email"}
+							setValue={setUserDetails}
+							type={"email"}
+							value={userDetails}
+							name={"email"}
+						/>
+						<Input
+							placeHolder={"Password"}
+							setValue={setUserDetails}
+							type={"password"}
+							value={userDetails}
+							name={"password"}
+						/>
+
+						<Input
+							placeHolder={"Confirm Password"}
+							setValue={setUserDetails}
+							type={"password"}
+							value={userDetails}
+							name={"confirmPassword"}
+						/>
+						{/* <img src="./hide.png" alt="" /> */}
+						<button className={styles.signBtn}>Sign Up</button>
+						<label className={styles.checkboxContainer}>
+							<input
+								type="checkbox"
+								checked={remember}
+								onChange={() => setRemember(!remember)}
+							/>
+							<span className={styles.checkmark}></span>
+							Remember Me
+						</label>
+						<div className={styles.new}>
+							<p>
+								Already have an account? <NavLink>Sign in</NavLink>
+							</p>
+							<p>
+								This page is protected by Google reCAPTCHA to ensure you’re not
+								a bot.<a href="#"> Learn more.</a>
+							</p>
+						</div>
+					</form>
 					{/* </div> */}
+				</div>
+				<div className={styles.footerD}>
+					<div>
+						<a href="">FAQ</a>
+						<a href="">Legal notices</a>
+						<a href="">Cookies</a>
+						<a href="">Ways to watch</a>
+					</div>
+
+					<div>
+						<a href="">Terms of use</a>
+						<a href="">Contact us</a>
+						<a href="">Help center</a>
+						<a href="">New Lin</a>
+					</div>
+					<div>
+						<a href="">Account</a>
+						<a href="">Privacy</a>
+						<a href="">Legal notices</a>
+						{/* <a href="">N</a> */}
+					</div>
 				</div>
 			</div>
 		</section>
 	);
 }
 
-export default SignUp 
+export default SignUp;
